0d16151e9d3b0e25b781aa208825e53b0bc86744
Al <alexkhien@hotmail.com>
2021-02-23 11:24:03 -0500

    Added Frontpage component

---
+++ frontend/src/components/Frontpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style.css'
+
+const Frontpage = () => {
+    return (
+        <h1 className='title'>
+            Sparkling Fresh Water
+        </h1>
+    )
+}
+
+export default Frontpage;
