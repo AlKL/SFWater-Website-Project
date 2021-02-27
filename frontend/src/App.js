e252b84cef90c835069a2fe99c4ce31bc36c9bad
Al <alexkhien@hotmail.com>
2021-02-27 17:16:24 -0500

    imported noramlize.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Frontpage from './components/Frontpage';
 import AddComment from './components/AddCommentModal/CommentIndex';
+import xBanner from './images/xBanner.png';
 
 const App = () => {
   return (
     <div>
+      <div className="bannerContainer">
+        <img src={xBanner} className='banner' />
+      </div>
+
+
+
+
       <Frontpage />
       <AddComment />
     </div>
