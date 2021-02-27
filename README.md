4bc80499adc8347254c2d6cc772cda95edd2cd38
Al <alexkhien@hotmail.com>
2021-02-27 09:19:53 -0500

    Installed Mongoose and successful conn test

--- README.md
+++ README.md
@@ -2,15 +2,22 @@
 
 To-do:
 - Complete middleware/logger/errorHandler
-- - Error handler should forward to an error page
+- - Unknown endpoint should forward to an error page
 
 Next steps:
-- Deploy to Heroku
-- Ensure it works in development mode and production
 - Connect to Mongo
 - Ensure Mongo does what you want it to do - text/email notifications?
+- Do frontend styling
+- remove ping
 
 git subtree push --prefix backend heroku main 
 // pushes to Heroku
+// only need to deploy when finished
 
-Remove ping
+
+
+Brainstorm:
+- you just use Node.js to send the e-mail. 1. Send comment to database 2. Send comment in e-mail template to inbox
+    - use nodemailer for this
+- Host on Heroku and use apps to keep it awake/add CC/choose time to sleep if necessary
+- Think of good domain name
---
+++ backend/mongoTest.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+// if (process.argv.length < 3) {
+//     console.log('Please provide the password as an argument: node mongo.js <password>');
+//     process.exit(1);
+// }
+
+// const password = process.argv[2];
+
+const url =
+    'mongodb+srv://alex:32185246aA@commentsubmissions.wq4gv.mongodb.net/test?retryWrites=true&w=majority';
+
+// `mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/test?retryWrites=true`;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean,
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+const note = new Note({
+    content: 'HTML is Easy',
+    date: new Date(),
+    important: true,
+});
+
+note.save().then(() => {
+    console.log('note saved!');
+    mongoose.connection.close();
+});
