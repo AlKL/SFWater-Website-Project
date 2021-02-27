e1254b3b36ea88f70cd25b25b92feb5e5a5fedf5
Al <alexkhien@hotmail.com>
2021-02-27 17:38:36 -0500

    Added some content and removed Frontpage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import Frontpage from './components/Frontpage';
+// import Frontpage from './components/Frontpage';
 import AddComment from './components/AddCommentModal/CommentIndex';
 import xBanner from './images/xBanner.png';
 
 const App = () => {
   return (
-    <div>
-      <div className="bannerContainer">
-        <img src={xBanner} className='banner' />
-      </div>
+    <div className='wrapper'>
 
 
 
+      <header>
+        <div className='bannerContainer'>
+          <img src={xBanner} className='banner' />
+        </div>
+        <div className='contactContainer'>
+          <p>Phone: (416) 241-7866</p>
+          <AddComment />
+        </div>
+      </header>
 
-      <Frontpage />
-      <AddComment />
+      <section>
+        <div className='pricesContainer'>
+          <p>New bottle: $13</p>
+          <p>Refill: $3</p>
+        </div>
+        <div className='mapContainer'>
+          <p>Map here</p>
+          <p>1722 Jane Street</p>
+          <p>Closest intersection: Jane & Lawrence</p>
+        </div>
+      </section>
+
     </div>
   );
 };
