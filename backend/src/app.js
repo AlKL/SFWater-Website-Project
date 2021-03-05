0e64ced14255a5810ed591c2f970fcf5e23afe85
Al <alexkhien@hotmail.com>
2021-03-05 16:44:05 -0500

    Finished 404 template and removed ping

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,10 +22,10 @@ app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 
-app.get('/api/ping', (_req, res) => {
-    console.log('someone pinged here');
-    res.send('pong');
-});
+// app.get('/api/ping', (_req, res) => {
+//     console.log('someone pinged here');
+//     res.send('pong');
+// });
 
 app.use('/api/comments', commentsRouter);
 
