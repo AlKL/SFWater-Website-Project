3894c581e07031cbc30dbddbe1072af59bba7673
Al <alexkhien@hotmail.com>
2021-03-04 22:55:39 -0500

    Fixed Card-Flipping issue

--- frontend/src/components/FlipCard/FlipCard.js
+++ frontend/src/components/FlipCard/FlipCard.js
@@ -12,27 +12,27 @@ const FlipCard = ({ image, backImage, desc }) => {
     return (
         <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
             <div className='flip-card'>
-                {/* <p>FRONT OF CARD</p> */}
-                <button onClick={handleFlip} className='flip-btn'>
+                <div onClick={handleFlip} className='flip-btn'>
                     <div className='card-text'>
                         <p>{desc}</p>
                     </div>
                     <div className='card-img'>
                         <img src={image}></img>
                     </div>
-                </button>
+                </div>
             </div>
 
             <div className='flip-card'>
-                {/* <p>BACK OF CARD</p> */}
-                <button onClick={handleFlip} className='flip-btn'>
-                    <div className='card-text'>
-                        <p>BACK</p>
+                <div>
+                    <div onClick={handleFlip} className='flip-btn'>
+                        <div className='card-text'>
+                            <p>BACK</p>
+                        </div>
+                        <div className='card-img'>
+                            <img src={backImage}></img>
+                        </div>
                     </div>
-                    <div className='card-img'>
-                        <img src={backImage}></img>
-                    </div>
-                </button>
+                </div>
             </div>
         </ReactCardFlip>
     );
