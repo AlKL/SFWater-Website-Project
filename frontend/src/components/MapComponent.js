f30dfb6e4c44b1ad3da25235af364bd2b17af549
Al <alexkhien@hotmail.com>
2021-03-28 18:19:19 -0400

    Fixed Meta Tags

---
+++ frontend/src/components/MapComponent.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker } from 'react-google-maps';
+
+const MapComponent = () => {
+
+    const Map = () => {
+        return (
+            <GoogleMap
+                defaultZoom={16}
+                defaultCenter={{ lat: 43.704030, lng: -79.504400 }}
+            >
+                <Marker
+                    position={{ lat: 43.704030, lng: -79.504400 }}
+                />
+            </GoogleMap>
+        );
+    };
+
+    const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+    return (
+        <div style={{ height: '100%', width: '100%' }} >
+            <WrappedMap
+                // eslint-disable-next-line no-undef
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+                loadingElement={<div style={{ height: '100%' }} />}
+                containerElement={<div style={{ height: '100%' }} />}
+                mapElement={<div style={{ height: '100%' }} />}
+            />
+        </div>
+    );
+};
+
+export default MapComponent;
